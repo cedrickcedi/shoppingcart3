d1f04f470bdd12c783a0014f650c7555313047d2
cedrickcedi <cedrick_cedi@yahoo.com>
2022-10-19 04:39:22 +0800

    commit

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+//** document.querySelector('#app').innerHTML = `
+//** <h1>Hello Vite!</h1>
+//**  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+//**` 
+
+let shop = document.getElementById("shop");
+
+console.log(shop);
+
+let generateShop = ()=>{
+  return ``
+}
